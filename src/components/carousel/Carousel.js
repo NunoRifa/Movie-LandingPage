2acce0790c1c931b642ce54a6c17ba332823c755
Nuno Rifa <nuno180202@gmail.com>
2022-10-08 19:10:03 +0700

    Make Component Carousel

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,12 @@
+import MovieInfo from "../movie-info/MovieInfo";
+import "./Carousel.css";
+
+const Carousel = () => {
+  return (
+    <div className="carousel__personal">
+      <MovieInfo />
+    </div>
+  );
+};
+
+export default Carousel;
